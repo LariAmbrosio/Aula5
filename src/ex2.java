6b3be2300696561fa7e08ff219e2dacedad70a8c
Larissa <lamariotto21@yahoo.com.br>
2020-07-21 21:47:11 -0400

    commit inicial

---
+++ src/ex2.java
@@ -0,0 +1,22 @@
+public class ex2 {
+    public static void main(String[] args) {
+
+        // declaração de vetor
+        int[] vetor = new int[20];
+
+        // declaração de variáveis
+        int i = 0, soma = 0;
+
+
+
+        // zerando o valor de i
+        i = i+0;
+
+        // imprimindo os números ímpares
+        for(; i < vetor.length; i++){
+            System.out.println(vetor[i]);
+        }
+    } // fim main
+} // fim class
+
+
